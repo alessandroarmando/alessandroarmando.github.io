22e0cbe51782ee4724fb9cc43120e3ad49d3738b
Alessandro Armando <alessandro.armando@unige.it>
2018-04-12 10:37:09 +0200

    *** empty log message ***

---
+++ code/thread_create_ok.c
@@ -0,0 +1,28 @@
+#include <pthread.h>
+#include <stdio.h>
+
+#define N 1000
+
+void* print_xs (void* c) {
+  char *pch=(char *)c; // type casting
+  int i;
+  for(i=0;i<N;i++)
+    fputc (*pch, stdout);
+  return NULL;
+}
+
+int main () {
+  int i;
+  pthread_t thread_id;
+  char ch='x';
+  char *pchar=&ch;
+  
+  pthread_create(&thread_id, NULL, &print_xs, pchar);
+  
+  for(i=0;i<N;i++)
+    fputc ('.', stdout);
+  
+  pthread_join(thread_id,NULL);
+
+  return 0;
+}
