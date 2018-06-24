bc91aea3ecef4ee3776523fca03377f18dca5700
Alessandro Armando <alessandro.armando@unige.it>
2018-06-24 20:30:03 +0200

    *** empty log message ***

--- code/socket_client.c
+++ code/socket_client.c
@@ -7,16 +7,22 @@
 #include <sys/socket.h>
 #include <sysexits.h>
 #include <unistd.h>
+#include <memory.h>
+#include "aes.h"
  
 #define PORT 8888
 #define BUF_SIZE 2048
- 
- 
-int main(int argc , char *argv[])
-{
+
+void print_hex(BYTE str[], int len) {
+  int idx;
+  for(idx = 0; idx < len; idx++)
+    printf("%02x", str[idx]);
+}
+
+int main(int argc , char *argv[]) {
     int sock;
     struct sockaddr_in server;
-    char message[BUF_SIZE], server_reply[BUF_SIZE];
+    BYTE message[BUF_SIZE], server_reply[BUF_SIZE];
      
     // create socket
     sock = socket(AF_INET , SOCK_STREAM , 0);
@@ -40,33 +46,57 @@ int main(int argc , char *argv[])
     puts("Connected\n");
      
     // keep communicating with server
-    while(true) {
-        // reset buffers
-        memset(message, '\0', sizeof(message));
-        memset(server_reply, '\0', sizeof(server_reply));
-        
-        int i;
-        for(i=0; i<BUF_SIZE; i++) {
-        server_reply[i] = '\0';
-        message[i] = '\0';
-        }
-        
-        printf("Enter message: ");
-        fgets(message, sizeof(message), stdin);
-         
-        if( send(sock, message, strlen(message), 0) == -1) {
-            perror("Error");
-			exit(EX_OSERR);
-        }
-         
-        // receive a reply from the server
-        if( recv(sock, server_reply, BUF_SIZE, 0) == -1) {
-			perror("Error");
-        	exit(EX_OSERR);
-        }
-        puts("Server reply:");
-        printf("%s", server_reply);
+    // reset buffers
+    memset(message, '\0', sizeof(message));
+    memset(server_reply, '\0', sizeof(server_reply));
+
+    const BYTE client_id=0x01;
+    const BYTE server_id=0xF0;
+    
+    /* printf("Sending Request-1:%02x %02x\n",client_id,server_id); */
+    message[0]=client_id;
+    message[1]=server_id;
+    printf("Sending Request-1:");
+    print_hex(message,2);
+    printf("\n");
+    
+    if( send(sock, message, 2, 0) == -1) {
+      perror("Error sending 1st message.");
+      exit(EX_OSERR);
     }
+    
+    // receive a reply from the server
+    if( recv(sock, server_reply, BUF_SIZE, 0) == -1) {
+      perror("Error");
+      exit(EX_OSERR);
+    }
+    printf("Server Reply:");
+    print_hex(server_reply,3);
+    printf("\n");
+
+    WORD key_schedule[60], idx;
+    BYTE enc_buf[128];
+    /* BYTE plaintext[16]; */
+    BYTE key[32] = {0x60,0x3d,0xeb,0x10,0x15,0xca,0x71,0xbe,0x2b,0x73,0xae,0xf0,0x85,0x7d,0x77,0x81,0x1f,0x35,0x2c,0x07,0x3b,0x61,0x08,0xd7,0x2d,0x98,0x10,0xa3,0x09,0x14,0xdf,0xf4};
+    printf("* Setting up the key:\n");
+    aes_key_setup(key, key_schedule, 256);
+    printf(  "Key          : ");
+    print_hex(key, 32);
+    
+    aes_encrypt(server_reply, enc_buf, key_schedule, 256);
+    printf("\nPlaintext    : ");
+    print_hex(server_reply, 16);
+    printf("\n-encrypted to: ");
+    print_hex(enc_buf, 16);
+    printf("\n");
+
+    if( send(sock, enc_buf, 16, 0) == -1) {
+      perror("Error sending response to challenge.");
+      exit(EX_OSERR);
+    }
+
+    printf("Response to challenge sent.\n");
+    
     close(sock);
     return 0;
 }
--- code/socket_server.c
+++ code/socket_server.c
@@ -7,17 +7,23 @@
 #include <sysexits.h>
 #include <unistd.h>
 #include <unistd.h>
+#include <memory.h>
+#include "aes.h"
 
 #define BUF_SIZE 2048
 #define MAX_CONN 1
 #define PORT 8888
 
+void print_hex(BYTE str[], int len) {
+  int idx;
+  for(idx = 0; idx < len; idx++)
+    printf("%02x", str[idx]);
+}
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     int socket_desc, client_sock, read_size, size_sockaddr;
     struct sockaddr_in server , client;
-    char client_message[BUF_SIZE];
+    BYTE client_message[BUF_SIZE];
 
     // create socket descriptor
     socket_desc = socket(AF_INET , SOCK_STREAM , 0);
@@ -45,7 +51,7 @@ int main(int argc, char *argv[])
     if (listen(socket_desc, MAX_CONN) != 0) {
         perror("Listen failed");
         exit(EX_OSERR);
-	}
+    }
 
     // accept and incoming connection
     puts("Waiting for incoming connections...");
@@ -57,16 +63,65 @@ int main(int argc, char *argv[])
         perror("Error");
         exit(EX_OSERR);
     }
-    
     puts("Connection accepted");
-    memset(client_message, '\0', sizeof(client_message));
 
     // receive a message from client
-    while( (read_size = recv(client_sock, client_message, BUF_SIZE, 0)) > 0 ) {
-    	client_message[BUF_SIZE-1] = '\0';
-        //Send the message back to client
-        write(client_sock , client_message , strlen(client_message));
-        memset(client_message, '\0', sizeof(client_message));
+    if((read_size = recv(client_sock, client_message, BUF_SIZE, 0)) > 0 ) {
+      printf("read_size:%d\n",read_size);
+      printf("Received Request:");
+      print_hex(client_message,read_size);
+      printf("\n");
+
+      int randomData = open("/dev/urandom", O_RDONLY);
+      if (randomData < 0) {
+        perror("Error opening /dev/urandom.");
+        exit(EX_OSERR);
+      }
+      else {
+	char myRandomData[1];
+	ssize_t result = read(randomData, myRandomData, sizeof myRandomData);
+	if (result < 0)
+	  {
+	    perror("Reading /dev/urandom failed.");
+	    exit(EX_OSERR);
+	  }
+	//Add challenge (nonce) to message
+	//      client_message[2]=0x23;
+	client_message[2]=myRandomData[0];
+      }      
+
+      //Send message back to client
+      write(client_sock,client_message,3);
+
+      //Receive response to challenge from client
+      if((read_size = recv(client_sock, client_message, BUF_SIZE, 0)) > 0 ) {
+      	printf("read_size:%d\n",read_size);
+	printf("Received Response:");
+	print_hex(client_message,read_size);
+	printf("\n");
+
+	BYTE plaintext[BUF_SIZE];
+	WORD key_schedule[60];
+	BYTE key[32] = {0x60,0x3d,0xeb,0x10,0x15,0xca,0x71,0xbe,0x2b,0x73,0xae,0xf0,0x85,0x7d,0x77,0x81,0x1f,0x35,0x2c,0x07,0x3b,0x61,0x08,0xd7,0x2d,0x98,0x10,0xa3,0x09,0x14,0xdf,0xf4};
+	printf("* Setting up the key:\n");
+	aes_key_setup(key, key_schedule, 256);
+	printf("Key          : ");
+	print_hex(key, 32);
+	printf("Decrypting Response:");
+	aes_decrypt(client_message, plaintext, key_schedule, 256);
+	printf("Decrypted Text:");
+	print_hex(plaintext,256);
+	printf("\n");
+      }
+      if(read_size == 0) {
+        puts("Client disconnected");
+        fflush(stdout);
+      }
+      else if(read_size == -1) {
+        perror("Error");
+        exit(EX_OSERR);
+      }
+      
     }
 
     if(read_size == 0) {
